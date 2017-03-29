a3c0c64865f0e3e61f653afed0127b214d60f3e3
kennethlumalicay <kennethlumalicay@gmail.com>
2017-03-29 18:26:42 -0400

    added get /

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require("express");
 var app = express();
-
+app.use(express.static(__dirname + "/view"));
+app.get("/", function(req, res) {
+	res.sendFile("index.html");
+});
 app.listen(process.env.PORT || 3000);
